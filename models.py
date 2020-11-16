d511342b1a90d59b083dfca73775c75a96d92693
julianaprado <50370069+julianaprado@users.noreply.github.com>
2020-11-16 12:29:35 -0300

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Livro(models.Model):
-    titulo = models.CharField(max_length=50)
-    data_publicacao = models.DateField(null=True)
-    autor = models.CharField(max_length=30, blank=True)
-    preco = models.DecimalField(max_digits=5, decimal_places=2)
-    quantidade_exemplares = models.IntegerField()
